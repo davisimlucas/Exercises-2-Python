181910e26b14636454b38a95553c6e0abeafc432
davisimlucas <davisimoeslucas@gmail.com>
2023-02-28 14:55:20 -0300

    functions and read file .txt

--- ex30_PickWord.py
+++ ex30_PickWord.py
@@ -6,23 +6,26 @@ Each line in the file contains a single word.
 '''
 import random
 
-# gerar lista vazia que receberá todos os nomes do arquivo txt 
-words = []
+def pickWord():
+    # gerar lista vazia que receberá todos os nomes do arquivo txt 
+    words = []
 
-# leitura de todos os nomes que estão no arquivo txt
-with open('ex30_sowpods.txt') as f:
-    # definir a variável line que lerá o nome que estão no arquivo txt
-    # readline() retorna o valor da linha
-    # strip() retorna uma cópia de string
-    line = f.readline().strip()
-    while line:
+    # leitura de todos os nomes que estão no arquivo txt
+    with open('ex30_sowpods.txt') as f:
+        # definir a variável line que lerá o nome que estão no arquivo txt
+        # readline() retorna o valor da linha
+        # strip() retorna uma cópia de string
         line = f.readline().strip()
-        words.append(line)
+        while line:
+            line = f.readline().strip()
+            words.append(line)
 
-# número que vai representar o index da list words sendo sorteado entre 0 e o tamanho (len()) da list 
-randomWord = random.randint(0, len(words))
+    # número que vai representar o index da list words sendo sorteado entre 0 e o tamanho (len()) da list 
+    randomWord = random.randint(0, len(words))
+    pickedWord = words[randomWord]
+    
+    return pickedWord
 
-pickedWord = words[randomWord]
 
-print('picked word is: {}'.format(pickedWord))
+
 
