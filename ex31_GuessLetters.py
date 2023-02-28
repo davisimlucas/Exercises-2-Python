2775b832074ee03621d7ee65e1cdaf54b16ae852
davisimlucas <davisimoeslucas@gmail.com>
2023-02-28 18:14:46 -0300

    functions

--- ex31_GuessLetters.py
+++ ex31_GuessLetters.py
@@ -10,42 +10,29 @@ we will deal with those in a future exercise.
 '''
 
 from ex30_PickWord import pickWord
-
-# fazer com que a palavra sorteada venha do ex 30 
+ 
 word = pickWord()
-# definir função com parâmetro variável
 def hangman(variable):
-    # transformar cada letra da palavra numa lista de strings
     wordList = list(variable)
     attempts = 0
-    # gerar lista que recebe elementos vazios ("_") do tamanho da lista que está a palavra
     spaceWord = ['_' for i in range(len(wordList))]
     # for looping que itera index e acompanha cada elemento da lista da palavra com a função enumerate()
-    for index, letter in enumerate(wordList):
-        # laço while para repetir a jogada até que não exista mais espaços vazios ("_") na list spaceWord 
+    for index, letter in enumerate(wordList): 
         while spaceWord.count("_") != 0:
-            # input para o usuário inserir uma letra
-            # diretamente essa letra é convertida em maiúsculas
             letterInput = str(input('Enter a letter: ')).upper()
-            # estrutura condicional para verificar se a letra inserida consta na lista da palavra sorteada
+
             if letterInput in wordList:
                 # se constar: fazer iterar cada posição (i) acompanhado a letra (l) com a função enumerate() na lista da palavra 
                 for i, l in enumerate(wordList):
-                    # estrutura condicional para verificar se a letra l na lista e na posição(i) é igual a letra inserida
                     if l == letterInput:
                         # se for: o elemento naquela posição(i) deixa de ser "_" e se recebe agora letra inserida 
                         spaceWord[i] = letterInput
-                # caso a letra inserida consta na lista da palavra
-                print('You pick a right letter!\n')
-                # função join() para imprimir a cada letra inserida a evolução do usuário no jogo
-                print(''.join(map(str, spaceWord)))
-            # caso a letra inserida não conste na lista da palavra 
+                print(f'You pick a right letter!\n\
+                    {"".join(map(str, spaceWord))}')
+
             elif letterInput not in wordList:
                 print('Incorrect! Try again.\n') 
             attempts += 1
-    # caso exista algum "_" na lista spaceWord, sai do laço while 
+            
     print(f'You got the word right! With {attempts} attempts\n')
               
-
-
-
